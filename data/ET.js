be6a3b1dfe63c510bc5f8d042f210944c4f41b27
kbstt <80958930+kbstt@users.noreply.github.com>
2021-08-27 11:55:03 +0800

    Add files via upload

--- data/ET.js
+++ data/ET.js
@@ -1,1 +1,1 @@
-window.dataPlaceHistory["ET"] = {"id":"ET","history":[{"d":0,"day":"20200313","i":2,"r":0}],"lastUpdated":"2020-03-13T17:29:04.756Z"};
+window.dataPlaceHistory["ET"] = {"id":"ET","lastUpdated":"2020-03-13T17:29:04.756Z","history":[{"d":0,"r":0,"day":"20200313","i":2}]};
