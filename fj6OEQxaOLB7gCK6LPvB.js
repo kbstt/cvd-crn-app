1a1b6706e80ae84383356f06da367d94c69aefa1
kbstt <80958930+kbstt@users.noreply.github.com>
2021-08-31 09:37:17 +0800

    Add files via upload

---
+++ fj6OEQxaOLB7gCK6LPvB.js
@@ -0,0 +1,1 @@
+window.dataPlaceHistory["fj6OEQxaOLB7gCK6LPvB"] = {"id":"fj6OEQxaOLB7gCK6LPvB","name":"Cabo Verde","latitude":15.1067,"pop":554000,"history":[{"r":0,"d":1,"day":"20200330","i":6},{"day":"20200329","r":0,"i":6,"d":1}],"lastUpdated":"2020-03-30T00:49:49.784Z","country":"CV","longitude":-23.6167};
