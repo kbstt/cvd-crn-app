ce3f6dffd175a41614c106b413cc664e517cf8dc
kbstt <80958930+kbstt@users.noreply.github.com>
2021-10-06 08:16:10 +0800

    Add files via upload

--- data/wRRAnVyBfBnlezTl9Fyo.js
+++ data/wRRAnVyBfBnlezTl9Fyo.js
@@ -1,1 +1,1 @@
-window.dataPlaceHistory["wRRAnVyBfBnlezTl9Fyo"] = {"id":"wRRAnVyBfBnlezTl9Fyo","latitude":41.806472,"name":"Cook County, IL","longitude":-87.767127,"history":[{"d":0,"r":2,"i":4,"day":"20200303"}],"lastUpdated":"2020-03-03T15:00:24.832Z","country":"US"};
+window.dataPlaceHistory["wRRAnVyBfBnlezTl9Fyo"] = {"id":"wRRAnVyBfBnlezTl9Fyo","history":[{"i":4,"day":"20200303","r":2,"d":0}],"latitude":41.806472,"lastUpdated":"2020-03-03T15:00:24.832Z","country":"US","longitude":-87.767127,"name":"Cook County, IL"};
