62c8e107ae43a0059531f3b78ba48303e1cd72cc
kbstt <80958930+kbstt@users.noreply.github.com>
2022-02-04 09:35:37 +0800

    Add files via upload

--- data/Q0Oa7BGmt3G7CJvkEL0r.js
+++ data/Q0Oa7BGmt3G7CJvkEL0r.js
@@ -1,1 +1,1 @@
-window.dataPlaceHistory["Q0Oa7BGmt3G7CJvkEL0r"] = {"id":"Q0Oa7BGmt3G7CJvkEL0r","history":[{"r":0,"i":1,"day":"20200307","d":0}],"lastUpdated":"2020-03-07T14:37:38.534Z","hide":false,"name":"Nebraska","country":"US","latitude":41.459666,"longitude":-99.450189};
+window.dataPlaceHistory["Q0Oa7BGmt3G7CJvkEL0r"] = {"id":"Q0Oa7BGmt3G7CJvkEL0r","longitude":-99.450189,"history":[{"day":"20200307","i":1,"r":0,"d":0}],"lastUpdated":"2020-03-07T14:37:38.534Z","country":"US","latitude":41.459666,"hide":false,"name":"Nebraska"};
--- data/SWOMh49moK80NwP1IAnz.js
+++ data/SWOMh49moK80NwP1IAnz.js
@@ -1,1 +1,1 @@
-window.dataPlaceHistory["SWOMh49moK80NwP1IAnz"] = {"id":"SWOMh49moK80NwP1IAnz","lastUpdated":"2020-03-06T15:18:46.423Z","name":"Serbia","longitude":20.935766,"hide":false,"country":"RS","latitude":44.154695,"history":[{"day":"20200306","d":0,"r":0,"i":1}]};
+window.dataPlaceHistory["SWOMh49moK80NwP1IAnz"] = {"id":"SWOMh49moK80NwP1IAnz","hide":false,"country":"RS","lastUpdated":"2020-03-06T15:18:46.423Z","history":[{"day":"20200306","r":0,"i":1,"d":0}],"name":"Serbia","latitude":44.154695,"longitude":20.935766};
