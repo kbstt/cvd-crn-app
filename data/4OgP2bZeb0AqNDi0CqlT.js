1e1a842dbc1850707fa2b8fef045b2e778ed21ff
kbstt <80958930+kbstt@users.noreply.github.com>
2022-02-20 11:06:45 +0800

    Add files via upload

--- data/4OgP2bZeb0AqNDi0CqlT.js
+++ data/4OgP2bZeb0AqNDi0CqlT.js
@@ -1,1 +1,1 @@
-window.dataPlaceHistory["4OgP2bZeb0AqNDi0CqlT"] = {"id":"4OgP2bZeb0AqNDi0CqlT","name":"Brunei","country":"BN","lastUpdated":"2020-03-11T14:16:06.421Z","hide":false,"longitude":114.7277,"latitude":4.5353,"history":[{"i":6,"r":0,"day":"20200311","d":0}]};
+window.dataPlaceHistory["4OgP2bZeb0AqNDi0CqlT"] = {"id":"4OgP2bZeb0AqNDi0CqlT","name":"Brunei","hide":false,"lastUpdated":"2020-03-11T14:16:06.421Z","history":[{"i":6,"d":0,"day":"20200311","r":0}],"longitude":114.7277,"latitude":4.5353,"country":"BN"};
