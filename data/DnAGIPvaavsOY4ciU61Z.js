c133db6cd98fba58d1fa61595a1dcaaa6a7375a6
kbstt <80958930+kbstt@users.noreply.github.com>
2022-04-24 10:40:09 +0800

    Add files via upload

--- data/DnAGIPvaavsOY4ciU61Z.js
+++ data/DnAGIPvaavsOY4ciU61Z.js
@@ -1,1 +1,1 @@
-window.dataPlaceHistory["DnAGIPvaavsOY4ciU61Z"] = {"id":"DnAGIPvaavsOY4ciU61Z","latitude":9.145,"country":"ET","longitude":40.4897,"name":"Ethiopia","lastUpdated":"2020-03-13T17:15:26.651Z","history":[{"i":1,"d":0,"day":"20200313","r":0}]};
+window.dataPlaceHistory["DnAGIPvaavsOY4ciU61Z"] = {"id":"DnAGIPvaavsOY4ciU61Z","latitude":9.145,"longitude":40.4897,"country":"ET","lastUpdated":"2020-03-13T17:15:26.651Z","history":[{"i":1,"r":0,"day":"20200313","d":0}],"name":"Ethiopia"};
