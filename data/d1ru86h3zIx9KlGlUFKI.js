914bd9fe19db9d798bee5829d707641d2087042d
kbstt <80958930+kbstt@users.noreply.github.com>
2022-06-11 08:42:07 +0800

    Add files via upload

--- data/d1ru86h3zIx9KlGlUFKI.js
+++ data/d1ru86h3zIx9KlGlUFKI.js
@@ -1,1 +1,1 @@
-window.dataPlaceHistory["d1ru86h3zIx9KlGlUFKI"] = {"id":"d1ru86h3zIx9KlGlUFKI","latitude":42.218852,"name":"Norfolk County, MA","lastUpdated":"2020-03-03T15:51:12.371Z","longitude":-71.067535,"country":"US","history":[{"d":0,"day":"20200303","i":1,"r":0}]};
+window.dataPlaceHistory["d1ru86h3zIx9KlGlUFKI"] = {"id":"d1ru86h3zIx9KlGlUFKI","history":[{"d":0,"i":1,"r":0,"day":"20200303"}],"longitude":-71.067535,"latitude":42.218852,"name":"Norfolk County, MA","country":"US","lastUpdated":"2020-03-03T15:51:12.371Z"};
