a16b3c337b0027cd2e8084384b368118a15844bd
kbstt <80958930+kbstt@users.noreply.github.com>
2022-08-21 10:36:04 +0800

    Add files via upload

--- data/Q0Oa7BGmt3G7CJvkEL0r.js
+++ data/Q0Oa7BGmt3G7CJvkEL0r.js
@@ -1,1 +1,1 @@
-window.dataPlaceHistory["Q0Oa7BGmt3G7CJvkEL0r"] = {"id":"Q0Oa7BGmt3G7CJvkEL0r","lastUpdated":"2020-03-07T14:37:38.534Z","hide":false,"name":"Nebraska","history":[{"d":0,"i":1,"day":"20200307","r":0}],"longitude":-99.450189,"latitude":41.459666,"country":"US"};
+window.dataPlaceHistory["Q0Oa7BGmt3G7CJvkEL0r"] = {"id":"Q0Oa7BGmt3G7CJvkEL0r","hide":false,"latitude":41.459666,"lastUpdated":"2020-03-07T14:37:38.534Z","country":"US","longitude":-99.450189,"name":"Nebraska","history":[{"day":"20200307","d":0,"r":0,"i":1}]};
--- data/d1ru86h3zIx9KlGlUFKI.js
+++ data/d1ru86h3zIx9KlGlUFKI.js
@@ -1,1 +1,1 @@
-window.dataPlaceHistory["d1ru86h3zIx9KlGlUFKI"] = {"id":"d1ru86h3zIx9KlGlUFKI","lastUpdated":"2020-03-03T15:51:12.371Z","latitude":42.218852,"name":"Norfolk County, MA","history":[{"i":1,"d":0,"day":"20200303","r":0}],"longitude":-71.067535,"country":"US"};
+window.dataPlaceHistory["d1ru86h3zIx9KlGlUFKI"] = {"id":"d1ru86h3zIx9KlGlUFKI","latitude":42.218852,"name":"Norfolk County, MA","longitude":-71.067535,"country":"US","lastUpdated":"2020-03-03T15:51:12.371Z","history":[{"day":"20200303","r":0,"d":0,"i":1}]};
