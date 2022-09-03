1471210c1f00442f1ae4234f1b2b5b9eadc1a9e6
kbstt <80958930+kbstt@users.noreply.github.com>
2022-09-03 08:35:33 +0800

    Add files via upload

--- data/ET.js
+++ data/ET.js
@@ -1,1 +1,1 @@
-window.dataPlaceHistory["ET"] = {"id":"ET","lastUpdated":"2020-03-13T17:29:04.756Z","history":[{"d":0,"day":"20200313","r":0,"i":2}]};
+window.dataPlaceHistory["ET"] = {"id":"ET","history":[{"d":0,"i":2,"day":"20200313","r":0}],"lastUpdated":"2020-03-13T17:29:04.756Z"};
