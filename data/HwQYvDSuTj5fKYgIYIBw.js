7bf66fb811e2084da9bf9a2ee8208213924391b8
kbstt <80958930+kbstt@users.noreply.github.com>
2022-09-07 08:39:16 +0800

    Add files via upload

--- data/HwQYvDSuTj5fKYgIYIBw.js
+++ data/HwQYvDSuTj5fKYgIYIBw.js
@@ -1,1 +1,1 @@
-window.dataPlaceHistory["HwQYvDSuTj5fKYgIYIBw"] = {"id":"HwQYvDSuTj5fKYgIYIBw","latitude":45.528074,"country":"US","history":[{"i":2,"day":"20200303","r":0,"d":0}],"name":"Washington County, OR","longitude":-123.0379,"lastUpdated":"2020-03-03T10:27:11.034Z"};
+window.dataPlaceHistory["HwQYvDSuTj5fKYgIYIBw"] = {"id":"HwQYvDSuTj5fKYgIYIBw","name":"Washington County, OR","longitude":-123.0379,"history":[{"i":2,"r":0,"d":0,"day":"20200303"}],"latitude":45.528074,"lastUpdated":"2020-03-03T10:27:11.034Z","country":"US"};
