dddad0235a7033eb1b0407cb1b3228d44b8752d4
kbstt <80958930+kbstt@users.noreply.github.com>
2022-09-27 08:45:34 +0800

    Add files via upload

--- data/SWOMh49moK80NwP1IAnz.js
+++ data/SWOMh49moK80NwP1IAnz.js
@@ -1,1 +1,1 @@
-window.dataPlaceHistory["SWOMh49moK80NwP1IAnz"] = {"id":"SWOMh49moK80NwP1IAnz","name":"Serbia","lastUpdated":"2020-03-06T15:18:46.423Z","latitude":44.154695,"longitude":20.935766,"country":"RS","hide":false,"history":[{"day":"20200306","r":0,"i":1,"d":0}]};
+window.dataPlaceHistory["SWOMh49moK80NwP1IAnz"] = {"id":"SWOMh49moK80NwP1IAnz","lastUpdated":"2020-03-06T15:18:46.423Z","longitude":20.935766,"name":"Serbia","history":[{"day":"20200306","d":0,"r":0,"i":1}],"hide":false,"country":"RS","latitude":44.154695};
