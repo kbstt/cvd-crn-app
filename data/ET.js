c678e7d5d1bc86fed37f307107da6b30b3b45031
kbstt <80958930+kbstt@users.noreply.github.com>
2022-10-14 08:28:31 +0800

    Add files via upload

--- data/ET.js
+++ data/ET.js
@@ -1,1 +1,1 @@
-window.dataPlaceHistory["ET"] = {"id":"ET","history":[{"i":2,"r":0,"d":0,"day":"20200313"}],"lastUpdated":"2020-03-13T17:29:04.756Z"};
+window.dataPlaceHistory["ET"] = {"id":"ET","lastUpdated":"2020-03-13T17:29:04.756Z","history":[{"i":2,"r":0,"d":0,"day":"20200313"}]};
--- data/HwQYvDSuTj5fKYgIYIBw.js
+++ data/HwQYvDSuTj5fKYgIYIBw.js
@@ -1,1 +1,1 @@
-window.dataPlaceHistory["HwQYvDSuTj5fKYgIYIBw"] = {"id":"HwQYvDSuTj5fKYgIYIBw","latitude":45.528074,"name":"Washington County, OR","history":[{"r":0,"d":0,"day":"20200303","i":2}],"lastUpdated":"2020-03-03T10:27:11.034Z","longitude":-123.0379,"country":"US"};
+window.dataPlaceHistory["HwQYvDSuTj5fKYgIYIBw"] = {"id":"HwQYvDSuTj5fKYgIYIBw","history":[{"i":2,"d":0,"r":0,"day":"20200303"}],"name":"Washington County, OR","longitude":-123.0379,"country":"US","latitude":45.528074,"lastUpdated":"2020-03-03T10:27:11.034Z"};
