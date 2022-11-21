ac6b3ce8c5f44087ece7237a67d2a0fef05ff9c7
kbstt <80958930+kbstt@users.noreply.github.com>
2022-11-21 08:45:09 +0800

    Add files via upload

--- data/Q0Oa7BGmt3G7CJvkEL0r.js
+++ data/Q0Oa7BGmt3G7CJvkEL0r.js
@@ -1,1 +1,1 @@
-window.dataPlaceHistory["Q0Oa7BGmt3G7CJvkEL0r"] = {"id":"Q0Oa7BGmt3G7CJvkEL0r","longitude":-99.450189,"lastUpdated":"2020-03-07T14:37:38.534Z","history":[{"r":0,"d":0,"i":1,"day":"20200307"}],"hide":false,"country":"US","name":"Nebraska","latitude":41.459666};
+window.dataPlaceHistory["Q0Oa7BGmt3G7CJvkEL0r"] = {"id":"Q0Oa7BGmt3G7CJvkEL0r","name":"Nebraska","history":[{"d":0,"day":"20200307","i":1,"r":0}],"hide":false,"longitude":-99.450189,"country":"US","latitude":41.459666,"lastUpdated":"2020-03-07T14:37:38.534Z"};
