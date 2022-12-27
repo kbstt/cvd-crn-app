38852bdec5dcb660a0833a58eb123412e7e9ceaa
kbstt <80958930+kbstt@users.noreply.github.com>
2022-12-27 08:49:52 +0800

    Add files via upload

--- data/DnAGIPvaavsOY4ciU61Z.js
+++ data/DnAGIPvaavsOY4ciU61Z.js
@@ -1,1 +1,1 @@
-window.dataPlaceHistory["DnAGIPvaavsOY4ciU61Z"] = {"id":"DnAGIPvaavsOY4ciU61Z","longitude":40.4897,"latitude":9.145,"name":"Ethiopia","history":[{"day":"20200313","d":0,"i":1,"r":0}],"country":"ET","lastUpdated":"2020-03-13T17:15:26.651Z"};
+window.dataPlaceHistory["DnAGIPvaavsOY4ciU61Z"] = {"id":"DnAGIPvaavsOY4ciU61Z","name":"Ethiopia","longitude":40.4897,"country":"ET","lastUpdated":"2020-03-13T17:15:26.651Z","latitude":9.145,"history":[{"i":1,"d":0,"day":"20200313","r":0}]};
--- data/wRRAnVyBfBnlezTl9Fyo.js
+++ data/wRRAnVyBfBnlezTl9Fyo.js
@@ -1,1 +1,1 @@
-window.dataPlaceHistory["wRRAnVyBfBnlezTl9Fyo"] = {"id":"wRRAnVyBfBnlezTl9Fyo","history":[{"i":4,"d":0,"day":"20200303","r":2}],"longitude":-87.767127,"country":"US","name":"Cook County, IL","latitude":41.806472,"lastUpdated":"2020-03-03T15:00:24.832Z"};
+window.dataPlaceHistory["wRRAnVyBfBnlezTl9Fyo"] = {"id":"wRRAnVyBfBnlezTl9Fyo","longitude":-87.767127,"lastUpdated":"2020-03-03T15:00:24.832Z","latitude":41.806472,"history":[{"i":4,"day":"20200303","d":0,"r":2}],"country":"US","name":"Cook County, IL"};
